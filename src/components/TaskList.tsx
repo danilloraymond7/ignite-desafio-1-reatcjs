f18ea96b6150d0c4a0f04c60eeef183dbf5b473d
danilloraymond7 <33788284+danilloraymond7@users.noreply.github.com>
2021-09-06 21:09:10 -0300

    Update TaskList.tsx
    Tarefas de do desafios

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,27 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle)
+      setTasks([...tasks,{
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        isComplete: false
+      }])
+      setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(
+      tasks.map(item => 
+        item.id === id 
+        ? {...item, isComplete : true} 
+        : item 
+    ))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(i => i.id !== id))
   }
 
   return (
